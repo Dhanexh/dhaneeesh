d9573fe16befaa48c38bf1bbb733dfa903e66d9f
dhanesh <112035003+Dhanexh@users.noreply.github.com>
2024-12-06 11:22:54 +0530

    Add files via upload

---
+++ name.py
@@ -0,0 +1,8 @@
+from art import text2art
+
+# Create ASCII art for the name "Dhanesh"
+ascii_art = text2art("Dhanesh")
+ascii_art = text2art("Dhanesh", font='block')  # You can choose different fonts
+
+# Print the generated ASCII art
+print(ascii_art)
